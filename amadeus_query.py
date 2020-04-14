e8afe0bfd02aaadb6838331ae768a261047bf073
georgethoraninh <42872670+georgethoraninh@users.noreply.github.com>
2020-04-14 00:48:29 -0400

    API call from Amadeus

--- amadeus_query.py
+++ amadeus_query.py
@@ -9,22 +9,42 @@ usage:  python amadeus_query.py
 # import statements
 import os
 import ssl
+import requests
 from amadeus import Client, ResponseError, Location
-
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
+from functools import lru_cache
 
 amadeus_api_key = os.environ['AMADEUS_API_KEY']
 amadeus_api_secret = os.environ['AMADEUS_API_SECRET']
 
-# initilize amadeus client
-amadeus = Client(
-    client_id = amadeus_api_key,
-    client_secret = amadeus_api_secret,
-    ssl = ssl_context
-)
 
-response = amadeus.reference_data.locations.airports.get(longitude=0.1278, latitude=51.5074)
+auth_response = requests.post('https://api.amadeus.com/v1/security/oauth2/token',
+                              data={'grant_type': 'client_credentials',
+                                    'client_id': amadeus_api_key,
+                                    'client_secret': amadeus_api_secret})
+bearer = auth_response.json()['access_token']
+#print(bearer)
+# API has quotas, so better to cache everything
+@lru_cache(maxsize=2048)
+def call_api(url, **params):
+    full_url = f'https://api.amadeus.com/v1{url}'
+    response = requests.get(full_url,
+                            params=params,
+                            headers={'Authorization': f'Bearer {bearer}',
+                                     'Content-Type': 'application/vnd.amadeus+json'})
+    return response.json()
 
-print(response.data)
+response = call_api('/shopping/flight-offers',
+                origin='YYZ',
+                destination='NYC',
+                departureDate='2020-08-01',
+                returnDate='2020-08-15',
+                adults='1',
+                currency='CAD',
+                maxPrice='1000',
+                max='5'
+                )
+print(response.keys())
+
+print('*******')
+
+print(response.values())
