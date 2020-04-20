818835ec379d7ae135bef40fe79aaa11742d7e5e
georgethoraninh <42872670+georgethoraninh@users.noreply.github.com>
2020-04-20 19:29:04 -0400

    Made airport names lower case

--- rasa_bot/actions.py
+++ rasa_bot/actions.py
@@ -5,6 +5,30 @@ from rasa_sdk.forms import FormAction
 from rasa_sdk import Action
 from rasa_sdk.events import SlotSet
 
+import os
+import requests
+from amadeus import Client, ResponseError, Location
+from functools import lru_cache
+import time
+from datetime import datetime
+from dateutil import parser
+import pandas as pd
+
+# Amadeus variables
+amadeus_api_key = os.environ['AMADEUS_API_KEY']
+amadeus_api_secret = os.environ['AMADEUS_API_SECRET']
+auth_response = requests.post('https://api.amadeus.com/v1/security/oauth2/token',
+                                                            data={'grant_type': 'client_credentials',
+                                                                        'client_id': amadeus_api_key,
+                                                                        'client_secret': amadeus_api_secret})
+bearer = auth_response.json()['access_token']
+
+# Airport codes
+airport_df = pd.read_csv('clean_airports.csv')
+
+def _find_flight_offer(location: Text, resource: Text) -> List[Dict]:
+    """Returns json of flight offer matching the search criteria."""
+    return response
 
 class FlighttForm(FormAction):
 
@@ -59,6 +83,13 @@ class FlighttForm(FormAction):
     ) -> List[Dict]:
         """Define what the form has to do
             after all required slots are filled"""
+        destination = tracker.get_slot("destination")
+        origin = tracker.get_slot("origin")
+        depart_date = tracker.get_slot("depart_date")
+        return_date = tracker.get_slot("return_date")
+        budget = tracker.get_slot("budget")
+
+        flight_offer = _find_flight_offer(origin, destination, depart_date, return_date, budget)
 
         # utter submit template
         dispatcher.utter_message(text=tracker.get_slot("destination"))
