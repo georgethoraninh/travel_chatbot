21ffeaadd32e5462dd3ae04e4b0f7dac231183d2
georgethoraninh <42872670+georgethoraninh@users.noreply.github.com>
2020-04-21 23:46:01 -0400

    Removed files

--- slack_bot_1.py
+++
@@ -1,8 +0,0 @@
-import sys
-from slacker import Slacker
-
-slack = Slacker('xoxb-1047057471891-1053942767041-JrP0D9ZXVDiYUAQc5LOAmfEy')
-
-# Send a message to #general channel
-message = 'Yo what\'s up everyone?'
-slack.chat.post_message('#general', message)
